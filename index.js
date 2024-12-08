185ebd4cbd4937c20e0a8cbd6b8b2e8c39bfa91d
nguyenhongdien <132563944+nguyenhongdien@users.noreply.github.com>
2024-12-08 10:42:25 +0700

    v1.0.0

---
+++ index.js
@@ -0,0 +1,31 @@
+const guessedNumTxt = document.getElementById("guessedNumTxt");
+const numInp = document.getElementById("numInp");
+const submitBtn = document.getElementById("submitBtn");
+const notifyTxt = document.getElementById("notifyTxt");
+const reloadBtn = document.getElementById("reloadBtn");
+
+const randomNum = Math.ceil(Math.random() * 200);
+console.log(randomNum);
+
+submitBtn.onclick = function(){
+    let num = numInp.value;
+    if (num == "" || num == null){
+        notifyTxt.textContent = `You must enter a number!`;
+    } else if (num > randomNum) {
+        guessedNumTxt.textContent = `${num}`
+        notifyTxt.textContent = `The number is lower`;
+        notifyTxt.style.color = "rgba(209, 223, 82, 1)";
+    } else if (num < randomNum) {
+        guessedNumTxt.textContent = `${num}`
+        notifyTxt.textContent = `The number is higher`;
+        notifyTxt.style.color = "orange";
+    } else if (num == randomNum) {
+        guessedNumTxt.textContent = `${num}`
+        notifyTxt.textContent = `You guess it right!\nThe number is ${randomNum}`;
+        notifyTxt.style.color = "green";
+    }
+}
+
+reloadBtn.onclick = function(){
+    location.reload();
+}
